6af9b4a84a2b3f69ac4a937023f964fda31368c1
Adeel Ansari <adeel.ansari@ymail.com>
2016-03-01 09:06:40 +0500

    Added IdGenerator and a crude test for that.

---
+++ src/main/java/totient/brief/IdGeneratr.java
@@ -0,0 +1,75 @@
+package totient.brief;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public enum IdGeneratr {
+  ID_GEN;
+  
+  public final char[] DIC_62 = new char[]{'0', '1', '2', '3', '4', 
+    '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
+    'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+  
+  private final long BASE = DIC_62.length;
+  private final SecureRandom SEC_RAND = new SecureRandom();
+
+  public String generateId() {
+
+    long value = SEC_RAND.nextLong();
+    value *= (value < 0) ? -1 : 1;
+    List<Character> seq = new ArrayList<>();
+    int exponent = 1;
+    
+    long remaining = value;
+    while (true) {
+      long a = (long) Math.pow(BASE, exponent);
+      long b = remaining % a;
+      long c = (long) Math.pow(BASE, exponent - 1);
+      int d = (int) (b/c);
+
+      seq.add(DIC_62[d]);
+      remaining = remaining - b;
+
+      if (remaining == 0L) 
+        break;
+
+      exponent++;
+    }
+
+    return reverse(abbreviate(seq));
+  }
+
+  private List<Character> abbreviate(List<Character> seq) {
+    Random rand = ThreadLocalRandom.current();
+    int n;
+    int[] inds = new int[3];
+    n = rand.nextInt((999 - 102) + 1) + 102;
+    
+    inds[0] = n / 100;
+    int temp = n % 100;
+    inds[1] = temp / 10;
+    inds[2] = temp % 10;
+    Arrays.sort(inds);
+    
+    char c = (seq.size()> inds[2]) ? seq.remove(inds[2]) : seq.remove(seq.size()-1);
+    c = (seq.size()> inds[1]) ? seq.remove(inds[1]) : seq.remove(seq.size()-1);
+    c = (seq.size()> inds[0]) ? seq.remove(inds[0]) : seq.remove(seq.size()-1);
+    
+    return seq;
+  }
+
+  private String reverse(List<Character> seq) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = seq.size() - 1; i >= 0; i--) {
+      sb.append(seq.get(i));
+    }
+    
+    return sb.toString();
+  }
+}
---
+++ src/test/java/totient/brief/IdGeneratrTest.java
@@ -0,0 +1,33 @@
+package totient.brief;
+
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import static totient.brief.IdGeneratr.ID_GEN;
+
+public class IdGeneratrTest {
+
+  @Test
+  public void testGenerateId() throws NoSuchAlgorithmException {
+    Instant start = Instant.now();
+    List<String> ids = new ArrayList<>();
+    for (int i = 0; i < 100000; i++) {
+      String s = ID_GEN.generateId();
+      if (ids.contains(s)) {
+        Assert.fail("Duplicate generated");
+      } else if(s.length() > 8) {
+        Assert.fail("Too large");
+      } else {
+        ids.add(s);
+      }
+    }
+    
+    Instant end = Instant.now();
+    System.out.println(Duration.between(start, end));
+
+  }
+}
